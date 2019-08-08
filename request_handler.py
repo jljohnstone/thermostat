df7b863041dfbe3c97e009f61ddd0745ea4c3c63
Alan Scott <scottca@sfasu.edu>
2019-08-08 07:00:49 -0500

    Add a method for handling POST data

--- request_handler.py
+++ request_handler.py
@@ -20,18 +20,27 @@ class RequestHandler:
     def json_decode(self, data):
         return json.loads(data)
 
-    def request(self, path):
+    def request(self, path, method = "GET", data = None):
         url = self.generate_url(path.strip('/ '))
+        req = urllib.request.Request(url)
 
-        with urllib.request.urlopen(url) as response:
-            return self.json_decode(response.read())
+        if method == "POST":
+            req.add_header("Content-Type", "application/x-www-form-urlencoded")
+            d = urllib.parse.urlencode(data).encode("utf-8")
+            with urllib.request.urlopen(req, data=d) as response:
+                resp = response.read()
+                return resp
+
+        else:
+            with urllib.request.urlopen(url) as response:
+                return self.json_decode(response.read())
 
     def json_request(self, path, body):
         url = self.generate_url(path.strip('/ '))
         req = urllib.request.Request(url)
         req.add_header("Content-Type", "application/json")
-        json_data = json.dumps(body)
-        json_data_bytes = json_data.encode("utf-8")
+        # json_data = json.dumps(body)
+        json_data_bytes = json.dumps(body).encode("utf-8")
 
         with urllib.request.urlopen(url, json_data_bytes) as response:
             return self.json_decode(response.read())
