8e051fbb9a5ace1c0c2509b2bf741bd76e9325b1
Alan Scott <scottca@sfasu.edu>
2019-08-17 02:33:28 -0500

    Add install/uninstall functions

--- installer.sh
+++ installer.sh
@@ -64,12 +64,65 @@ EOF
 }
 
 
+create_env_local() {
+    local_env="${APP_DIRECTORY}.env.local"
+    cp ${APP_DIRECTORY}.env $local_env
+    sed -i '/^#/d' $local_env
+    new_comment="# This file should NOT be committed to a repository.\n# (See the .gitignore file.)"
+    sed -i "1s;^;${new_comment}\n;" $local_env
+}
+
+
 # Install the program.
 install() {
     check_user
     print_separator
-    printf "Installing\n"
+    printf "INSTALL THERMOSTAT MONITOR\n\n"
+    printf "Installing application to ${APP_DIRECTORY}\n\n"
 
+    printf "  * Creating application directory...\n"
+    mkdir -p ${APP_DIRECTORY}
+    if [ $? -ne 0 ]; then
+        printf "\nFailed to create application directory.\nExiting.\n\n"
+        exit 1
+    fi
+
+    printf "  * Installing application files...\n"
+    cp -rT ./src ${APP_DIRECTORY}
+    if [ $? -ne 0 ]; then
+        printf "\nFailed to install application files.\nExiting.\n\n"
+        exit 1
+    else
+        printf "  * Creating .env.local...\n"
+        create_env_local
+    fi
+
+    printf "  * Installing systemd service...\n"
+    cp ./etc/systemd/system/thermostat.service ${SYSTEMD_TARGET}
+    if [ $? -ne 0 ]; then
+        printf "\nFailed to install systemd service.\nExiting.\n\n"
+        exit 1
+    fi
+
+    printf "  * Applying permissions...\n"
+    chmod 644 ${SYSTEMD_TARGET}
+    if [ $? -ne 0 ]; then
+        printf "\nFailed to set systemd service permissions.\nExiting.\n\n"
+        exit 1
+    fi
+
+    printf "\nInstallation successful.\n"
+
+    print_separator
+    printf "NEXT STEPS\n\n"
+    printf "Edit the configuration settings in the \".env.local\" file:\n"
+    printf "  ${APP_DIRECTORY}.env.local\n\n"
+    printf "Enable the systemd service to run the thermostat monitor at startup:\n"
+    printf "  $ sudo systemctl enable thermostat\n\n"
+    printf "Issue the start and status commands to run and verify the service:\n"
+    printf "  $ sudo systemctl start thermostat\n"
+    printf "  $ systemctl status thermostat\n\n"
+    exit 0
 }
 
 
@@ -77,8 +130,28 @@ install() {
 uninstall() {
     check_user
     print_separator
-    printf "Uninstalling\n"
+    printf "UNINSTALL THERMOSTAT MONITOR\n\n"
 
+    printf "  * Stopping and disabling any running instance...\n"
+    systemctl stop thermostat
+    systemctl disable thermostat
+
+    printf "  * Removing application directory...\n"
+    rm -rf ${APP_DIRECTORY}
+    if [ $? -ne 0 ]; then
+        printf "\nFailed to remove the application directory.\nExiting.\n\n"
+        exit 1
+    fi
+
+    printf "  * Removing systemd configuration...\n"
+    rm ${SYSTEMD_TARGET}
+    if [ $? -ne 0 ]; then
+        printf "\nFailed to remove the systemd service configuration.\nExiting.\n\n"
+        exit 1
+    fi
+
+    printf "\nRemoval completed.\n\n"
+    exit 0
 }
 
 
