d0239982858e9551847bbf3f9657a477ca3520c5
Alan Scott <scottca@sfasu.edu>
2019-08-22 03:48:21 -0500

    Set class variables and use the updated RequestHandler

--- src/griddy.py
+++ src/griddy.py
@@ -6,27 +6,33 @@
 from request_handler import RequestHandler
 
 
-address = "app.gogriddy.com"
-path = "/api/v1/insights/getnow"
-
-
 class Griddy:
+
+    server = "app.gogriddy.com"
+    path = "/api/v1/insights/getnow"
 
     def __init__(self, meter_id, member_id, settlement_point):
         self.meter_id = meter_id
         self.member_id = member_id
         self.settlement_point = settlement_point
-        self.handler = RequestHandler(address)
-        self.handler.set_ssl(True)
         self.data = None
 
     def query(self):
-        json = {
+        json_data = {
             "meterid": self.meter_id,
             "memberid": self.member_id,
             "settlement_point": self.settlement_point
         }
-        self.data = self.handler.json_request(path, json)
+
+        handler = RequestHandler()
+        handler.set_server(self.server)
+        handler.set_path(self.path)
+        handler.set_data(json_data)
+
+        self.data = handler.send_request()
+
+    def get_data(self):
+        return self.data
 
     def get_current_status(self):
         return self.data["now"]
