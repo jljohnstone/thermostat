c0852d2bf97b7396c71006ba7d5fa4240870714c
Alan Scott <scottca@sfasu.edu>
2019-08-22 03:53:43 -0500

    Improve parameter handling and build the request from class properties

--- src/request_handler.py
+++ src/request_handler.py
@@ -10,41 +10,52 @@ import urllib.request
 
 class RequestHandler:
 
-    def __init__(self, server):
-        self.server = server.strip('/ ')
-        self.set_ssl(False)
-    
-    def set_ssl(self, is_ssl):
-        if is_ssl:
-            self.protocol = "https"
-        else:
-            self.protocol = "http"
+    def __init__(self):
+        self.use_ssl = True
+        self.server = None
+        self.path = None
+        self.data = None
+
+    def set_server(self, server):
+        self.server = server.strip("/ ")
+
+    def set_path(self, path):
+        trimmed_path = path.strip("/ ")
+
+        if trimmed_path != "":
+            self.path = "".join(["/", trimmed_path])
+
+    def set_data(self, data):
+        self.data = data
 
     def generate_url(self, path):
-        return "{}://{}/{}".format(self.protocol, self.server, path)
+        if self.use_ssl:
+            protocol = "https"
+        else:
+            protocol = "http"
+        return "{}://{}{}".format(protocol, self.server, path)
 
     def json_decode(self, data):
         return json.loads(data)
 
-    def request(self, path, method = "GET", data = None):
-        url = self.generate_url(path.strip('/ '))
+    def send_request(self, method = "GET"):
+        url = self.generate_url(self.path)
         req = urllib.request.Request(url)
 
         if method == "POST":
             req.add_header("Content-Type", "application/x-www-form-urlencoded")
-            d = urllib.parse.urlencode(data).encode("utf-8")
-            with urllib.request.urlopen(req, data=d) as response:
+            d = urllib.parse.urlencode(self.data).encode("utf-8")
+
+            with urllib.request.urlopen(req, data = d) as response:
                 resp = response.read()
                 return resp
         else:
-            with urllib.request.urlopen(url) as response:
+            req.add_header("Content-Type", "application/json")
+
+            if self.data:
+                json_data_bytes = json.dumps(self.data).encode("utf-8")
+            else:
+                json_data_bytes = None
+
+            with urllib.request.urlopen(url, json_data_bytes) as response:
                 return self.json_decode(response.read())
-
-    def json_request(self, path, body):
-        url = self.generate_url(path.strip('/ '))
-        req = urllib.request.Request(url)
-        req.add_header("Content-Type", "application/json")
-        json_data_bytes = json.dumps(body).encode("utf-8")
-
-        with urllib.request.urlopen(url, json_data_bytes) as response:
-            return self.json_decode(response.read())
