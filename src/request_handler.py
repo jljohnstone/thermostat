e70f7aa34d6159d0e0b7d53f9dc9638fccf87610
Alan Scott <scottca@sfasu.edu>
2019-08-25 02:25:40 -0500

    Use a fluent builder to create a handler and update how protocols are set

--- src/request_handler.py
+++ src/request_handler.py
@@ -10,30 +10,38 @@ import urllib.request
 
 class RequestHandler:
 
+    ALLOWED_PROTOCOLS = ("http", "https")
+
     def __init__(self):
-        self.use_ssl = True
+        self.protocol = "https"
         self.server = None
         self.path = None
         self.data = None
 
+    def set_protocol(self, protocol):
+        if protocol in self.ALLOWED_PROTOCOLS:
+            self.protocol = protocol
+            return self
+        else:
+            raise RequestHandlerException("An invalid protocol was specified.")
+
     def set_server(self, server):
         self.server = server.strip("/ ")
+        return self
 
     def set_path(self, path):
         trimmed_path = path.strip("/ ")
 
         if trimmed_path != "":
             self.path = "".join(["/", trimmed_path])
+        return self
 
     def set_data(self, data):
         self.data = data
+        return self
 
     def generate_url(self, path):
-        if self.use_ssl:
-            protocol = "https"
-        else:
-            protocol = "http"
-        return "{}://{}{}".format(protocol, self.server, path)
+        return "{}://{}{}".format(self.protocol, self.server, path)
 
     def json_decode(self, data):
         return json.loads(data)
@@ -59,3 +67,8 @@ class RequestHandler:
 
             with urllib.request.urlopen(url, json_data_bytes) as response:
                 return self.json_decode(response.read())
+
+
+class RequestHandlerException(Exception):
+    def __init__(self, message):
+        self.message = "RequestHandlerException: {}".format(message)
