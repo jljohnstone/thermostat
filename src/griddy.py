28f7b3328f01e5b072c75c253d49e63a4f3ccaf2
Alan Scott <scottca@sfasu.edu>
2019-09-07 00:31:28 -0500

    Manage price methods in the Griddy class and integrate logging

--- src/griddy.py
+++ src/griddy.py
@@ -4,18 +4,21 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 from request_handler import RequestHandler
+import log_manager
 
 
 class Griddy:
 
     SERVER = "app.gogriddy.com"
     PATH = "/api/v1/insights/getnow"
+    MAX_CENTS_PER_KWH = 5.0
 
     def __init__(self, meter_id, member_id, settlement_point):
         self.meter_id = meter_id
         self.member_id = member_id
         self.settlement_point = settlement_point
-        self.data = None
+        self.price = None
+        self.logger = log_manager.setup_logging(__name__)
 
     def query(self):
         json_data = {
@@ -24,7 +27,7 @@ class Griddy:
             "settlement_point": self.settlement_point
         }
 
-        self.data = (
+        data = (
             RequestHandler()
             .set_server(self.SERVER)
             .set_path(self.PATH)
@@ -32,5 +35,27 @@ class Griddy:
             .send_request()
         )
 
-    def get_current_price(self):
-        return self.data["now"]["price_ckwh"]
+        self.compare_price(data["now"]["price_ckwh"])
+
+        return data
+
+    def price_is_high(self, current_price):
+        return float(current_price) > float(self.MAX_CENTS_PER_KWH)
+
+    def compare_price(self, current_price):
+        if self.price != current_price:
+            if not self.price:
+                self.logger.info(
+                    "Initialized price: {}".format(self.format_price(current_price))
+                )
+            else:
+                self.logger.info(
+                    "Price changed: {} --> {}".format(
+                        self.format_price(self.price), self.format_price(current_price)
+                    )
+                )
+            self.price = current_price
+
+    def format_price(self, price):
+        return "{0:.6f}".format(float(price))
+
